3d8a2193a8e869f7f6b78e2fa3bb9281f150f29e
wemali05 <alfredwemali05@gmail.com>
2020-03-21 10:55:31 +0300

    Thread factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,8 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
-use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +24,15 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\Thread::class, function (Faker $faker) {
+    return [
+      'user_id' => function () {
+          return factory('App\User')->create()->id;
+      },
+      'title' => $faker->sentence,
+      'body' => $faker->paragraph
     ];
 });
