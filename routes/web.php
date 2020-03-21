3edbc1e4871a1e485ba324d7d979d277b6f322d7
wemali05 <alfredwemali05@gmail.com>
2020-03-21 13:19:26 +0300

    threads index

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadsController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
