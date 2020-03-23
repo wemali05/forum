d789d0b121c3693d118331be1afafedd301e346a
wemali05 <alfredwemali05@gmail.com>
2020-03-23 11:43:28 +0300

    reply thread form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,13 +19,31 @@
         </div>
     </div>
 
-       <div class="row justify-content-center mt-4">
+    <div class="row justify-content-center mt-4">
         <div class="col-md-8">
-        @foreach($thread->replies as $reply)
-        <!-- replies to a thread -->
-          @include('threads.reply')
-        @endforeach    
+            @foreach($thread->replies as $reply)
+            <!-- replies to a thread -->
+            @include('threads.reply')
+            @endforeach    
         </div>
     </div>
+
+    @if(auth()->check())
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-8">  
+               <form action="{{ $thread->path() }}/replies" method="POST">
+               @csrf 
+
+                    <div class="form-group">
+                            <textarea  class="form-control" name="body" id="body"  rows="5" placeholder="Have something to say?"></textarea>
+                    </div>
+                
+                    <button type="submit" class="btn btn-default">Post</button>
+               </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login')}}">sign in</a> to participate in discussion</p>
+    @endif
 </div>
 @endsection
