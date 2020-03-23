f88df21196217aa8a535fed6a6bc5771784409ad
wemali05 <alfredwemali05@gmail.com>
2020-03-23 11:41:59 +0300

    store replies

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
 
 Auth::routes();
 
