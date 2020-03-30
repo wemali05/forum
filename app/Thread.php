60f0c43f04d6a0db28f527d57a26a9f82e74dfcd
wemali05 <alfredwemali05@gmail.com>
2020-03-30 11:00:56 +0300

    chanel thread relationship

--- app/Thread.php
+++ app/Thread.php
@@ -10,7 +10,8 @@ class Thread extends Model
 
     public function path()
     {
-        return '/threads/' . $this->id;
+        return "/threads/{$this->channel->slug}/{$this->id}";
+        // return '/threads/' .$this->channel->slug . '/'. $this->id;
     }
 
     public function replies()
@@ -21,6 +22,11 @@ class Thread extends Model
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
     }
 
     public function addReply($reply)
