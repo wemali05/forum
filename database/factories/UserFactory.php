0c93bdf238405382194ad161c9ff1e58b388316c
wemali05 <alfredwemali05@gmail.com>
2020-03-30 11:02:42 +0300

    channel factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,10 +32,22 @@ $factory->define(App\Thread::class, function (Faker $faker) {
       'user_id' => function () {
           return factory('App\User')->create()->id;
       },
+      'channel_id' => function () {
+          return factory('App\Channel')->create()->id;
+      },
       'title' => $faker->sentence,
       'body' => $faker->paragraph
     ];
 });
+
+$factory->define(App\Channel::class, function (Faker $faker) {
+    $name = $faker->word;
+    return [
+      'name' => $name,
+      'slug' => $name
+    ];
+});
+
 
 $factory->define(App\Reply::class, function (Faker $faker) {
     return [
