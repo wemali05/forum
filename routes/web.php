24d858245ca4ec008cf445f9142968fa361e5009
wemali05 <alfredwemali05@gmail.com>
2020-04-01 13:46:16 +0300

    threads channel route

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
 // Route::resource('/threads', 'ThreadsController');
 
-
+Route::get('/threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
 Auth::routes();
