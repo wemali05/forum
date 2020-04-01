c388bf7d889f74aba58375b8e730531c1d427ac6
wemali05 <alfredwemali05@gmail.com>
2020-04-01 16:09:15 +0300

    extracting to view composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \View::share('channels', \App\Channel::all()) ;
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                                 Channels
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                 <a class="dropdown-item" href="/threads/{{$channel->slug}}">{{$channel->slug}}</a>
                                 @endforeach
                             </div>
