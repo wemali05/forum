1fefae60f28e83b32ae5bb7ca8983f8125096347
wemali05 <alfredwemali05@gmail.com>
2020-04-05 23:19:42 +0300

    updated navbar with all threads and my threads dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,17 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li>
-                            <a class="nav-link" href="/threads">All Threads</a>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Browse
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                               <li><a class="dropdown-item" href="/threads/">All Threads</a></li> 
+                               @if(auth()->check())
+                               <li><a class="dropdown-item" href="/threads?by={{ auth()->user()->name }}">My Threads</a></li> 
+                               @endif
+                            </ul>
                         </li>
 
                         <li>
